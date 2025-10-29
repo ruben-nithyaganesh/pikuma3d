2454b875984c01eb83b5dc14e5100630367c9ca8
Ruben <66393567+ruben-nithyaganesh@users.noreply.github.com>
2025-10-29 23:23:32 +1300

    sdl build

--- build/build.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p build
-gcc -Wall -std=c99 platform/sdl/platform.c src/*.c -I/opt/homebrew/include -I/opt/homebrew/include/ -L/opt/homebrew/lib -lSDL2 -D_THREAD_SAFE -o build/renderer
---
+++ build/sdl/build.sh
@@ -0,0 +1,1 @@
+gcc -Wall -std=c99 ../../platform/sdl/platform.c ../../src/*.c -I/opt/homebrew/include -I/opt/homebrew/include/ -L/opt/homebrew/lib -lSDL2 -D_THREAD_SAFE -o sdl_renderer
